0790b15ac9e80838703e7b012b19fccc6afb45e9
Erkin Alp Güney <erkinalp9035@gmail.com>
2014-12-20 17:16:26 +0200

    İçerik girildi, ayrıca çalışan/patron takip kısmı eklendi employee ve employment tabloları ile religion kısıtı eklendi

--- social.sql
+++ social.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Anamakine: localhost
--- Üretim Zamanı: 20 Ara 2014, 12:28:30
+-- Üretim Zamanı: 20 Ara 2014, 17:16:00
 -- Sunucu sürümü: 5.5.40-0ubuntu1
 -- PHP Sürümü: 5.5.12-2ubuntu5
 
@@ -27,8 +27,21 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `actors` (
-  `actor_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_unicode_ci;
+`actor_id` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=63 DEFAULT CHARSET=utf32 COLLATE=utf32_unicode_ci;
+
+--
+-- Tablo döküm verisi `actors`
+--
+
+INSERT INTO `actors` (`actor_id`) VALUES
+(1),
+(57),
+(58),
+(59),
+(60),
+(61),
+(62);
 
 -- --------------------------------------------------------
 
@@ -165,6 +178,7 @@ CREATE TABLE IF NOT EXISTS `country` (
 --
 
 INSERT INTO `country` (`id`, `country`) VALUES
+('KTC', 'Kuzey Kıbrıs Türk Cumhuriyeti'),
 ('TUR', 'Turkey'),
 ('USA', 'United States of America');
 
@@ -196,7 +210,36 @@ CREATE TABLE IF NOT EXISTS `emails` (
 --
 
 INSERT INTO `emails` (`email_id`, `email_textual`) VALUES
-(1, 'erkinalp9035@gmail.com');
+(1, 'erkinalp9035@gmail.com'),
+(2, 'alperbicer@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `employer`
+--
+
+CREATE TABLE IF NOT EXISTS `employer` (
+`employer_id` int(11) NOT NULL,
+  `employer_user` int(11) NOT NULL,
+  `employer_page` int(11) NOT NULL,
+  `description` text COLLATE utf32_turkish_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_turkish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `employment`
+--
+
+CREATE TABLE IF NOT EXISTS `employment` (
+  `employment_id` int(11) NOT NULL,
+  `employer` int(11) NOT NULL,
+  `employee` int(11) NOT NULL,
+  `date_started` int(11) NOT NULL,
+  `date_quit` int(11) DEFAULT NULL,
+  `fired` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -318,13 +361,20 @@ INSERT INTO `gender` (`gender_id`, `gender`) VALUES
 CREATE TABLE IF NOT EXISTS `group` (
   `group_id` int(11) NOT NULL,
   `privacy` int(11) NOT NULL,
-  `created_by` int(11) NOT NULL,
+  `created_by` int(11) DEFAULT '57',
   `name` varchar(80) COLLATE utf32_turkish_ci NOT NULL,
   `description` text COLLATE utf32_turkish_ci NOT NULL,
   `created_on` date NOT NULL,
-  `destroyed_on` date NOT NULL,
+  `destroyed_on` date DEFAULT NULL,
   `last_actvity_happened` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_turkish_ci;
+
+--
+-- Tablo döküm verisi `group`
+--
+
+INSERT INTO `group` (`group_id`, `privacy`, `created_by`, `name`, `description`, `created_on`, `destroyed_on`, `last_actvity_happened`) VALUES
+(60, 0, 57, 'Veritabanı Yönetimi Dersini Erkin Alp GÜNEY''den alanlar grubu', 'Bu grup, veritabanı yönetimi dersini Erkin Alp Güney''den alanlara adanmıştır.', '2014-12-20', NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -336,8 +386,15 @@ CREATE TABLE IF NOT EXISTS `group_membership` (
   `user_id` int(11) NOT NULL,
   `group_id` int(11) NOT NULL,
   `membership_start` date NOT NULL,
-  `membershipe_end` date NOT NULL
+  `membership_end` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_turkish_ci;
+
+--
+-- Tablo döküm verisi `group_membership`
+--
+
+INSERT INTO `group_membership` (`user_id`, `group_id`, `membership_start`, `membership_end`) VALUES
+(57, 60, '2012-12-21', NULL);
 
 -- --------------------------------------------------------
 
@@ -359,9 +416,15 @@ CREATE TABLE IF NOT EXISTS `interests` (
 
 CREATE TABLE IF NOT EXISTS `lang` (
   `id` int(11) NOT NULL,
-  `language` varchar(50) COLLATE utf32_turkish_ci NOT NULL,
-  `user_id` int(11) NOT NULL
+  `language` varchar(50) COLLATE utf32_turkish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_turkish_ci;
+
+--
+-- Tablo döküm verisi `lang`
+--
+
+INSERT INTO `lang` (`id`, `language`) VALUES
+(1, 'Türkçe');
 
 -- --------------------------------------------------------
 
@@ -424,10 +487,21 @@ CREATE TABLE IF NOT EXISTS `notification` (
 
 CREATE TABLE IF NOT EXISTS `page_generic` (
   `pagenumber` int(11) NOT NULL,
-  `date_created` int(11) NOT NULL,
+  `date_created` date NOT NULL,
   `summary` varchar(255) COLLATE utf32_turkish_ci NOT NULL,
-  `description` text COLLATE utf32_turkish_ci
+  `description` text COLLATE utf32_turkish_ci,
+  `privacy` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_turkish_ci;
+
+--
+-- Tablo döküm verisi `page_generic`
+--
+
+INSERT INTO `page_generic` (`pagenumber`, `date_created`, `summary`, `description`, `privacy`) VALUES
+(0, '2012-12-21', 'Public People', 'This is an anonymous page to cover privacy setting public.', 0),
+(59, '2014-12-20', 'Ege Üniversitesi', 'Ege Üniversitesi, 1955 yılında Muhiddin EREL öncülüğünde kurulmuş bir Türk yükseköğretim okuludur.', 0),
+(61, '0571-04-07', 'Islam', NULL, 0),
+(62, '2012-12-21', 'Religion of Mozilla', 'What you see in about:mozilla on Firefox is our book. We believe in free software and free web''s power.', 0);
 
 -- --------------------------------------------------------
 
@@ -547,20 +621,21 @@ CREATE TABLE IF NOT EXISTS `user_info` (
   `middlename` varchar(50) COLLATE utf32_turkish_ci DEFAULT NULL,
   `familyname` varchar(50) COLLATE utf32_turkish_ci DEFAULT NULL,
   `gender` int(11) NOT NULL,
-  `marital_status_code` int(11) NOT NULL,
+  `marital_status_code` smallint(6) NOT NULL,
   `quit` tinyint(1) NOT NULL DEFAULT '0',
   `address` int(11) NOT NULL,
   `religion` int(11) DEFAULT NULL,
   `rating` int(11) NOT NULL,
-  `mail` int(11) NOT NULL
+  `mail` int(11) NOT NULL,
+  `lang` int(11) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_turkish_ci;
 
 --
 -- Tablo döküm verisi `user_info`
 --
 
-INSERT INTO `user_info` (`user_id`, `date_joined`, `date_of_birth`, `givenname`, `middlename`, `familyname`, `gender`, `marital_status_code`, `quit`, `address`, `religion`, `rating`, `mail`) VALUES
-(57, '2012-12-21', '1994-12-27', 'Erkin', 'Alp', 'Güney', 2, 0, 0, 1, NULL, 0, 1);
+INSERT INTO `user_info` (`user_id`, `date_joined`, `date_of_birth`, `givenname`, `middlename`, `familyname`, `gender`, `marital_status_code`, `quit`, `address`, `religion`, `rating`, `mail`, `lang`) VALUES
+(57, '2012-12-21', '1994-12-27', 'Erkin', 'Alp', 'Güney', 2, 0, 0, 1, NULL, 0, 1, 1);
 
 --
 -- Dökümü yapılmış tablolar için indeksler
@@ -627,6 +702,18 @@ ALTER TABLE `emails`
  ADD PRIMARY KEY (`email_id`);
 
 --
+-- Tablo için indeksler `employer`
+--
+ALTER TABLE `employer`
+ ADD PRIMARY KEY (`employer_id`), ADD UNIQUE KEY `employer_user_2` (`employer_user`,`employer_page`), ADD KEY `employer_user` (`employer_user`), ADD KEY `employer_company` (`employer_page`);
+
+--
+-- Tablo için indeksler `employment`
+--
+ALTER TABLE `employment`
+ ADD KEY `employer_ref` (`employer`), ADD KEY `employee` (`employee`);
+
+--
 -- Tablo için indeksler `fav_animals`
 --
 ALTER TABLE `fav_animals`
@@ -657,15 +744,33 @@ ALTER TABLE `friend_list`
  ADD PRIMARY KEY (`id`);
 
 --
+-- Tablo için indeksler `gender`
+--
+ALTER TABLE `gender`
+ ADD PRIMARY KEY (`gender_id`);
+
+--
+-- Tablo için indeksler `group`
+--
+ALTER TABLE `group`
+ ADD PRIMARY KEY (`group_id`), ADD KEY `created_by` (`created_by`);
+
+--
 -- Tablo için indeksler `group_membership`
 --
 ALTER TABLE `group_membership`
- ADD KEY `group_id` (`group_id`);
+ ADD PRIMARY KEY (`user_id`,`group_id`), ADD KEY `group_id` (`group_id`);
 
 --
 -- Tablo için indeksler `interests`
 --
 ALTER TABLE `interests`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Tablo için indeksler `lang`
+--
+ALTER TABLE `lang`
  ADD PRIMARY KEY (`id`);
 
 --
@@ -744,7 +849,71 @@ ALTER TABLE `thumbs_up`
 -- Tablo için indeksler `user_info`
 --
 ALTER TABLE `user_info`
- ADD PRIMARY KEY (`user_id`);
+ ADD PRIMARY KEY (`user_id`), ADD KEY `mail` (`mail`) COMMENT 'user_email', ADD KEY `user_gender` (`gender`), ADD KEY `user_marital` (`marital_status_code`), ADD KEY `user_address` (`address`), ADD KEY `religion` (`religion`), ADD KEY `user_locale` (`lang`);
+
+--
+-- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
+--
+
+--
+-- Tablo için AUTO_INCREMENT değeri `actors`
+--
+ALTER TABLE `actors`
+MODIFY `actor_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=63;
+--
+-- Tablo için AUTO_INCREMENT değeri `employer`
+--
+ALTER TABLE `employer`
+MODIFY `employer_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Dökümü yapılmış tablolar için kısıtlamalar
+--
+
+--
+-- Tablo kısıtlamaları `employer`
+--
+ALTER TABLE `employer`
+ADD CONSTRAINT `employer_company` FOREIGN KEY (`employer_page`) REFERENCES `page_generic` (`pagenumber`),
+ADD CONSTRAINT `employer_user` FOREIGN KEY (`employer_user`) REFERENCES `user_info` (`user_id`) ON UPDATE CASCADE;
+
+--
+-- Tablo kısıtlamaları `employment`
+--
+ALTER TABLE `employment`
+ADD CONSTRAINT `employment_employer` FOREIGN KEY (`employer`) REFERENCES `employer` (`employer_id`),
+ADD CONSTRAINT `employee_ref` FOREIGN KEY (`employee`) REFERENCES `user_info` (`user_id`);
+
+--
+-- Tablo kısıtlamaları `group`
+--
+ALTER TABLE `group`
+ADD CONSTRAINT `group_actor` FOREIGN KEY (`group_id`) REFERENCES `actors` (`actor_id`),
+ADD CONSTRAINT `group_creator` FOREIGN KEY (`created_by`) REFERENCES `user_info` (`user_id`);
+
+--
+-- Tablo kısıtlamaları `group_membership`
+--
+ALTER TABLE `group_membership`
+ADD CONSTRAINT `group_ref` FOREIGN KEY (`group_id`) REFERENCES `group` (`group_id`),
+ADD CONSTRAINT `group_member` FOREIGN KEY (`user_id`) REFERENCES `user_info` (`user_id`);
+
+--
+-- Tablo kısıtlamaları `page_generic`
+--
+ALTER TABLE `page_generic`
+ADD CONSTRAINT `page_actor` FOREIGN KEY (`pagenumber`) REFERENCES `actors` (`actor_id`);
+
+--
+-- Tablo kısıtlamaları `user_info`
+--
+ALTER TABLE `user_info`
+ADD CONSTRAINT `user_lang` FOREIGN KEY (`lang`) REFERENCES `lang` (`id`),
+ADD CONSTRAINT `religion` FOREIGN KEY (`religion`) REFERENCES `page_generic` (`pagenumber`),
+ADD CONSTRAINT `user_actor` FOREIGN KEY (`user_id`) REFERENCES `actors` (`actor_id`) ON DELETE CASCADE,
+ADD CONSTRAINT `user_address` FOREIGN KEY (`address`) REFERENCES `address` (`address_id`),
+ADD CONSTRAINT `user_email` FOREIGN KEY (`mail`) REFERENCES `emails` (`email_id`) ON UPDATE CASCADE,
+ADD CONSTRAINT `user_gender` FOREIGN KEY (`gender`) REFERENCES `gender` (`gender_id`) ON UPDATE CASCADE,
+ADD CONSTRAINT `user_marital` FOREIGN KEY (`marital_status_code`) REFERENCES `marital_status` (`marital_status_id`) ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
